6ffd888e3388e0792e95ee1477b0d7b1a10c2774
aaaaabandeev <125127673+aaaaabandeev@users.noreply.github.com>
2023-02-25 14:48:54 +0600

    Create task.py

---
+++ episode1/task1/1.1/task.py
@@ -0,0 +1,47 @@
+import math
+# Пример 1
+def task_1(a, d, c):
+    result1 = ((c-d/5+math.sqrt(321))/(1+a*3))
+    return result1
+
+
+# Пример 2
+def task_2(a, d, c):
+    result2 = ((math.log10(c/3)-d+25)/(a*5-1))
+    return result2
+
+
+# Пример 3
+def task_3(a, d, c):
+    result3 = ((c/2+math.log(d)-35)/(a*5+1))
+    return result3
+
+
+# Пример 4
+def task_4(a, d, c):
+    result4 = ((math.tan(c)+d/32)/(a/3+1))
+    return result4
+
+
+# Пример 5
+def task_5(a, d, c):
+    result5 = ((c/5-d+1)/(c+math.tan(2*a)))
+    return result5
+
+
+# Пример 6
+def task_6(a, d, c):
+    result6 = ((math.sqrt(25*c)+d-3)/(d-a*a+1))
+    return result6
+
+
+# Пример 7
+def task_7(a, d, c):
+    result7 = ((c*d-math.log(4*3*a))/(c+a-1))
+    return result7
+
+
+# Пример 8
+def task_8(a, d, c):
+    result8 = ((c*d-math.log(4*3*a))/(c+a-1))
+    return result8
